a1ff348d1f36acf1cca65b341c766a539f6a7069
ROBOT <tkata12@outlook.jp>
2026-04-04 18:37:32 +0900

    add: jsObf func

---
+++ packages/core/src/index.ts
@@ -0,0 +1,11 @@
+import { obfuscate } from "javascript-obfuscator";
+
+/**
+ *
+ * this func is wrapper of javascript-obfuscator.
+ * I'll make it myself **someday**.
+ *
+ */
+export function jsObf(src: string) {
+  return obfuscate(src).getObfuscatedCode();
+}
